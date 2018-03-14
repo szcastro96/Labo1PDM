3ea4e27bc49ea794b4878655b4634c82a8b0e026
UCA <00071114@uca.edu.sv>
2018-03-13 18:19:49 -0600

    ELLA NO TE AMA x2

---
+++ app/src/main/java/com/example/acastro/labo1pdm/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.acastro.labo1pdm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
